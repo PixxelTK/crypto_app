2665ab73588bcb0707e79dfb85a7946786205bfb
Pirapat <pixxel.tk@gmail.com>
2026-08-11 10:16:46 +0700

    feat: update interactive state to coin card container

--- lib/features/coins/view/widgets/coin_card_container.dart
+++ lib/features/coins/view/widgets/coin_card_container.dart
@@ -2,7 +2,7 @@ import 'package:crypto_app/style/tokens/colors.dart';
 import 'package:crypto_app/style/tokens/radius.dart';
 import 'package:flutter/material.dart';
 
-class CoinCardContainer extends StatelessWidget {
+class CoinCardContainer extends StatefulWidget {
   const CoinCardContainer({
     super.key,
     required this.coinColor,
@@ -13,6 +13,7 @@ class CoinCardContainer extends StatelessWidget {
     this.gradientStops = const [0.0, 0.50, 1.0],
     this.gradientBegin = Alignment.topLeft,
     this.gradientEnd = Alignment.bottomRight,
+    this.onTap,
   });
 
   final Color? coinColor;
@@ -23,35 +24,83 @@ class CoinCardContainer extends StatelessWidget {
   final List<double> gradientStops;
   final AlignmentGeometry gradientBegin;
   final AlignmentGeometry gradientEnd;
+  final VoidCallback? onTap;
+
+  @override
+  State<CoinCardContainer> createState() => _CoinCardContainerState();
+}
+
+class _CoinCardContainerState extends State<CoinCardContainer> {
+  bool _isHovered = false;
+  bool _isPressed = false;
+
+  Color _getSurfaceColor(BuildContext context) {
+    final baseColor = context.colors.surface;
+    if (_isPressed) {
+      return Color.lerp(baseColor, Colors.black, 0.1) ?? baseColor;
+    } else if (_isHovered) {
+      return Color.lerp(baseColor, Colors.black, 0.05) ?? baseColor;
+    }
+    return baseColor;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: margin,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(AppRadius.lg)),
-        color: context.colors.surface,
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(AppRadius.lg)),
-          gradient: coinColor == null
-              ? null
-              : LinearGradient(
-                  begin: gradientBegin,
-                  end: gradientEnd,
-                  stops: gradientStops,
-                  colors: [
-                    coinColor!.withValues(alpha: gradientAlphas[0]),
-                    coinColor!.withValues(alpha: gradientAlphas[1]),
-                    coinColor!.withValues(alpha: gradientAlphas[2]),
-                    if (gradientAlphas.length > 3)
-                      coinColor!.withValues(alpha: gradientAlphas[3]),
-                  ].take(gradientStops.length).toList(),
+      margin: widget.margin,
+      child: MouseRegion(
+        onEnter: (_) => setState(() => _isHovered = true),
+        onExit: (_) => setState(() => _isHovered = false),
+        cursor: widget.onTap != null
+            ? SystemMouseCursors.click
+            : MouseCursor.defer,
+        child: GestureDetector(
+          onTapDown: (_) => setState(() => _isPressed = true),
+          onTapUp: (_) => setState(() => _isPressed = false),
+          onTapCancel: () => setState(() => _isPressed = false),
+          onTap: widget.onTap,
+          child: AnimatedScale(
+            scale: _isPressed ? 0.96 : 1.0,
+            duration: const Duration(milliseconds: 150),
+            curve: Curves.easeOutCubic,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(AppRadius.lg)),
+                color: _getSurfaceColor(context),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(AppRadius.lg)),
+                  gradient: widget.coinColor == null
+                      ? null
+                      : LinearGradient(
+                          begin: widget.gradientBegin,
+                          end: widget.gradientEnd,
+                          stops: widget.gradientStops,
+                          colors: [
+                            widget.coinColor!.withValues(
+                              alpha: widget.gradientAlphas[0],
+                            ),
+                            widget.coinColor!.withValues(
+                              alpha: widget.gradientAlphas[1],
+                            ),
+                            widget.coinColor!.withValues(
+                              alpha: widget.gradientAlphas[2],
+                            ),
+                            if (widget.gradientAlphas.length > 3)
+                              widget.coinColor!.withValues(
+                                alpha: widget.gradientAlphas[3],
+                              ),
+                          ].take(widget.gradientStops.length).toList(),
+                        ),
                 ),
+                padding: widget.padding,
+                child: widget.child,
+              ),
+            ),
+          ),
         ),
-        padding: padding,
-        child: child,
       ),
     );
   }
